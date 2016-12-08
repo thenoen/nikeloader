918acbde75a6f6ff69b1d48a28a39cdb4fffe2f2
marek <kordikmarek@gmail.com>
2016-12-08 21:31:09 +0100

    updated memory settings for prod. run

--- start-prod.sh
+++ start-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar target/nikeloader-0.0.1-SNAPSHOT.jar --spring.config.location=configurations/
+java -Xms50m -Xmx50m -XX:MaxMetaspaceSize=50m -XX:-UseCompressedOops -XX:-UseCompressedClassPointers -XX:CompressedClassSpaceSize=1m -jar target/nikeloader-0.0.1-SNAPSHOT.jar --spring.config.location=configurations/
